cc9e909312c216e3d64d56464e11f79c03bfd2eb
Nadiia <wildfrenzy0432@gmail.com>
2023-06-08 23:22:40 +0200

    map validation done. i believe

--- parsing/parse.c
+++ parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: nmaliare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 15:37:26 by nmaliare          #+#    #+#             */
-/*   Updated: 2023/06/08 01:47:37 by nmaliare         ###   ########.fr       */
+/*   Updated: 2023/06/08 22:16:35 by nmaliare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 # include <fcntl.h>
 
 # include "get_next_line.h"
+
+//check if we need typedef for norm ?
+enum position
+{
+	first, middle, last
+};
 
 typedef struct s_player
 {
@@ -47,7 +53,7 @@ typedef struct s_map
 	t_player player;
 }				t_map;
 
-int	init_map(t_map *map)
+int		init_map(t_map *map)
 {
 	t_colour floor;
 	t_colour ceiling;
@@ -79,7 +85,7 @@ int	init_map(t_map *map)
 	return 1;
 }
 
-int	check_file(char *file)
+int		check_file(char *file)
 {
 	//add ft_
 	if(strncmp(file + strlen(file) - 4, ".cub", 4) != 0)
@@ -87,7 +93,7 @@ int	check_file(char *file)
 	return (1);
 }
 
-int	validate_map_line(char *line)
+int		validate_map_line(char *line)
 {
 	int i = -1;
 
@@ -101,7 +107,7 @@ int	validate_map_line(char *line)
 			return (printf("Error\nInvalid map!\n") & 0);
 		if (i > 0 && line[i-1] == ' ' && line[i] != '1' && line[i] != ' ') //if prev is space, next should be space or 1
 			return (printf("Error\nInvalid map!\n") & 0);
-		if (line[i + 1] == '\0' && line[i] != '1') //if next - eol, curr should be 1
+		if (line[i + 1] == '\0' && line[i] != '1' && line[i] != ' ') //if next - eol, curr should be 1 or space
 			return (printf("Error\nInvalid map!\n") & 0);
 		if (i > 0 && line[i] == ' ' && line[i - 1] != '1' && line[i - 1] != ' ') // prev of space can be only 1 or space
 			return (printf("Error\nInvalid map!\n") & 0);
@@ -110,7 +116,7 @@ int	validate_map_line(char *line)
 	return 1;
 }
 
-int	get_width_height(int fd, t_map *map)
+int		get_width_height(int fd, t_map *map)
 {
 	char *buf = NULL;
 	int len;
@@ -223,6 +229,7 @@ int		get_textures(int fd, t_map *map)
 	return 1;
 }
 
+/********** rgb **********/
 //r = 1, g = 2, b = 3
 int		check_rgb(char *rgb, int colour)
 {
@@ -337,6 +344,7 @@ int		check_colours(t_map *map)
 	}
 	return 1;
 }
+/*******************/
 
 int		scroll_to_map(int fd)
 {
@@ -352,6 +360,132 @@ int		scroll_to_map(int fd)
 	}
 	return 1;
 }
+
+void	print_map(t_map *map)
+{
+	int j = -1;
+	int all = map->mapX * map->mapY;
+	printf("\nMAP:\n");
+	while (++j < all)
+	{
+		if (j % (all / map->mapY) == 0)
+			printf("\n[%d]", map->map[j]);
+		else
+			printf("[%d]", map->map[j]);
+	}
+	printf("\n\nall[%d]\n", all);
+}
+
+/****** map ******/
+int		check_top(t_map *map, int i, int position)
+{
+	int	top;
+
+	top = i - map->mapX;
+	if (map->map[top] == 0)
+		return (printf("Error\nInvalid map - no top wall\n") & 0);
+	if (position != first && map->map[top - 1] == 0)
+		return (printf("Error\nInvalid map - no top-left wall\n") & 0);
+	if (position != last && map->map[top + 1] == 0)
+		return (printf("Error\nInvalid map - no top-right wall\n") & 0);
+	return 1;
+}
+
+int		check_bot(t_map *map, int i, int position)
+{
+	int	bot;
+
+	bot = i + map->mapX;
+	if (map->map[bot] == 0)
+		return (printf("Error\nInvalid map - no bot wall\n") & 0);
+	if (position != first && map->map[bot - 1] == 0)
+		return (printf("Error\nInvalid map - no bot-left wall\n") & 0);
+	if (position != last && map->map[bot + 1] == 0)
+		return (printf("Error\nInvalid map - no bot-right wall\n") & 0);
+	return 1;
+}
+
+int		check_first_row(t_map *map)
+{
+	int i = -1;
+	int all_spaces = 0;
+
+	while (++i < map->mapX)
+	{
+		if (map->map[i] == 0)
+			return (printf("Error\nInvalid map\n") & 0);
+		else if (map->map[i] == -1)
+		{
+			all_spaces += 1;
+			if (i == 0 && !check_bot(map, i, first))
+				return 0;
+			else if (i == map->mapX - 1 && !check_bot(map, i, last))
+				return 0;
+			else if (i != 0 && i != map->mapX - 1 && !check_bot(map, i, middle))
+				return 0;
+		}
+	}
+	if (all_spaces == map->mapX)
+		return (printf("Error\nInvalid map - first row all spaces\n") & 0);
+
+	return 1;
+}
+
+int		check_last_row(t_map *map)
+{
+	int i;
+	int total = map->mapX * map->mapY;
+	int all_spaces = 0;
+
+	i = (map->mapX * (map->mapY - 1)) - 1;
+	int tmp = i;
+
+	while (++i < total)
+	{
+		if (map->map[i] == 0)
+			return (printf("Error\nInvalid map\n") & 0);
+		else if (map->map[i] == -1)
+		{
+			all_spaces += 1;
+			if (i == tmp && !check_top(map, i, first))
+				return 0;
+			else if (i == tmp + map->mapX - 1 && !check_top(map, i, last))
+				return 0;
+			else if (i != tmp && i != tmp + map->mapX - 1 && !check_top(map, i, middle))
+				return 0;
+		}
+	}
+	if (all_spaces == map->mapX)
+		return (printf("Error\nInvalid map - last row all spaces\n") & 0);
+	return 1;
+}
+
+int		validate_walls(t_map *map)
+{
+	int i = map->mapX - 1; //last of 1st row
+	long size = map->mapX * (map->mapY - 1);
+
+	if (!check_first_row(map))
+		return 0;
+
+	//we start with 1 element 2nd row
+	while (++i < size)
+	{
+		if (map->map[i] == -1)
+		{
+			if (i % map->mapX == 0 && (!check_top(map, i, first) || !check_bot(map, i, first)))
+				return 0;
+			else if (i % map->mapX == map->mapX - 1 && (!check_top(map, i, last) || !check_bot(map, i, last)))
+				return 0;
+			else if (i % map->mapX != 0 && i % map->mapX != map->mapX - 1 && (!check_top(map, i, middle) || !check_bot(map, i, middle)))
+				return 0;
+		}
+	}
+	if (!check_last_row(map))
+		return 0;
+	return 1;
+}
+/************/
 
 int main(int ac, char *av[])
 {
@@ -422,18 +556,10 @@ int main(int ac, char *av[])
 
 	printf("player: x[%d], y[%d], direction[%c]\n", map.player.x, map.player.y, map.player.dir);
 
-// print map:
-	int j = -1;
-	int all = map.mapX * map.mapY;
-	printf("\nMAP:\n");
-	while (++j < all)
-	{
-		if (j % (all / map.mapY) == 0)
-			printf("\n[%d]", map.map[j]);
-		else
-			printf("[%d]", map.map[j]);
-	}
-	printf("\n\nall[%d]\n", all);
+	if (!validate_walls(&map))
+		return 0;
+
+	//print_map(&map);
 	return 0;
 }
 
