8f907d18c3e5ca11af56ba5e89427120505d0ca4
Barra MacMahon <barramacmah@gmail.com>
2023-07-07 17:02:33 +0200

    norm

--- textures.c
+++ textures.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   textures.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmaliare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: bmacmaho <bmacmaho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 23:48:41 by nmaliare          #+#    #+#             */
-/*   Updated: 2023/07/05 14:56:15 by nmaliare         ###   ########.fr       */
+/*   Updated: 2023/07/07 17:00:45 by bmacmaho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	load_texture(mlx_t	*mlx, mlx_image_t **img, char *file)
 	*img = mlx_texture_to_image(mlx, &(texture->texture));
 	printf("img: %dpx %dpx\n", (*img)->width, (*img)->height);
 	mlx_delete_xpm42(texture);
-	return 1;
+	return (1);
 }
 
 void	resize_textures(t_cub *cub)
@@ -39,25 +39,25 @@ void	resize_textures(t_cub *cub)
 int	prepare_textures(t_cub *cub)
 {
 	if (!load_texture(cub->mlx, &(cub->map.north), cub->map.no))
-		return 0;
+		return (0);
 	if (!load_texture(cub->mlx, &(cub->map.south), cub->map.so))
 	{
 		mlx_delete_image(cub->mlx, cub->map.north);
-		return 0;
+		return (0);
 	}
 	if (!load_texture(cub->mlx, &(cub->map.east), cub->map.ea))
 	{
 		mlx_delete_image(cub->mlx, cub->map.north);
 		mlx_delete_image(cub->mlx, cub->map.south);
-		return 0;
+		return (0);
 	}
 	if (!load_texture(cub->mlx, &(cub->map.west), cub->map.we))
 	{
 		mlx_delete_image(cub->mlx, cub->map.north);
 		mlx_delete_image(cub->mlx, cub->map.south);
 		mlx_delete_image(cub->mlx, cub->map.east);
-		return 0;
+		return (0);
 	}
 	//resize_textures(cub);
-	return 1;
+	return (1);
 }
