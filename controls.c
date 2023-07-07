cdc8ce6b4ae1ce26424b98d4b7931f6d8256595a
Esperanza <wildfrenzy0432@gmail.com>
2023-07-07 12:38:16 +0200

    moving smooth, not going through walls anymore

--- controls.c
+++ controls.c
@@ -6,7 +6,7 @@
 /*   By: bmacmaho <bmacmaho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 23:11:23 by bmacmaho          #+#    #+#             */
-/*   Updated: 2023/07/07 03:36:57 by nmaliare         ###   ########.fr       */
+/*   Updated: 2023/07/07 12:10:56 by nmaliare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,41 +28,44 @@ void	rotate(t_cub *cub, double rot_speed)
 	cub->player.plane.y = old_plane_x * sin(rot_speed) + cub->player.plane.y * cos(rot_speed);
 }
 
-//TODO wasd keys to move. arrows to change view
+//TODO wasd keys to move. arrows to change view only
 
 void	ft_hook(void *param)
 {
 	t_cub	*cub;
-	double move_speed = 0.042;
-	int x;
-	int y;
+	double move_speed = 0.06;
+	int x_access;
+	int y_access;
 
 	cub = param;
 	if (mlx_is_key_down(cub->mlx, MLX_KEY_ESCAPE))
 		ft_clean_exit(10, cub);
-	if (mlx_is_key_down(cub->mlx, MLX_KEY_UP)) //moving edgy
+
+	if (mlx_is_key_down(cub->mlx, MLX_KEY_UP))
 	{
-		t_dpoint new_up;
-
-		x = (int) (cub->player.pos.x + cub->player.dir.x * move_speed);
-		y = (int) (cub->player.pos.y + cub->player.dir.y * move_speed);
-
-		if (cub->map.map[(int)(cub->player.pos.y * cub->map.mapX + x)] == 0)
-			new_up.x = cub->player.pos.x + cub->player.dir.x * move_speed;
-		if (cub->map.map[(int)(y * cub->map.mapX + cub->player.pos.x)] == 0)
-			new_up.y = cub->player.pos.y + cub->player.dir.y * move_speed;
-		cub->player.pos = new_up;
+		x_access =
+				(int)(cub->player.pos.y) * cub->map.mapX +
+				(int)(cub->player.pos.x + cub->player.dir.x * (move_speed + 0.01));
+		y_access =
+				(int)(cub->player.pos.y + cub->player.dir.y * (move_speed + 0.01)) *
+				cub->map.mapX + (int)(cub->player.pos.x);
+		if (cub->map.map[x_access] == 0)
+			cub->player.pos.x += cub->player.dir.x * move_speed;
+		if (cub->map.map[y_access] == 0)
+			cub->player.pos.y += cub->player.dir.y * move_speed;
 	}
-	if (mlx_is_key_down(cub->mlx, MLX_KEY_DOWN)) //smooth move without ifs ;c
+	if (mlx_is_key_down(cub->mlx, MLX_KEY_DOWN))
 	{
-		t_dpoint new_down;
-		x = (int) (cub->player.pos.x - cub->player.dir.x * move_speed);
-		y = (int) (cub->player.pos.y - cub->player.dir.y * move_speed);
-		//if (cub->map.map[(int)(cub->player.pos.y * cub->map.mapX + x)] == 0)
-			new_down.x = cub->player.pos.x - cub->player.dir.x * move_speed;
-		//if (cub->map.map[(int)(y * cub->map.mapX + cub->player.pos.x)] == 0)
-			new_down.y = cub->player.pos.y - cub->player.dir.y * move_speed;
-		cub->player.pos = new_down;
+		x_access =
+				(int)(cub->player.pos.y) * cub->map.mapX +
+				(int)(cub->player.pos.x - cub->player.dir.x * (move_speed + 0.01));
+		y_access =
+				(int)(cub->player.pos.y - cub->player.dir.y * (move_speed + 0.01)) *
+				cub->map.mapX + (int)(cub->player.pos.x);
+		if (cub->map.map[x_access] == 0)
+			cub->player.pos.x -= cub->player.dir.x * move_speed;
+		if (cub->map.map[y_access] == 0)
+			cub->player.pos.y -= cub->player.dir.y * move_speed;
 	}
 	if (mlx_is_key_down(cub->mlx, MLX_KEY_LEFT))
 		rotate(cub, -ROT);
