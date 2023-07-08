f159b2659b805865033397c1cccd5579ca6b4383
Barra MacMahon <barramacmah@gmail.com>
2023-07-08 14:27:46 +0200

    free texture imgs

--- exit.c
+++ exit.c
@@ -6,7 +6,7 @@
 /*   By: bmacmaho <bmacmaho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 23:15:52 by barramacmah       #+#    #+#             */
-/*   Updated: 2023/07/08 13:36:34 by bmacmaho         ###   ########.fr       */
+/*   Updated: 2023/07/08 14:25:45 by bmacmaho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,13 @@
 int	ft_clean_exit(int stat, t_cub *cub)
 {
 	mlx_delete_image(cub->mlx, cub->img);
-	mlx_terminate(cub->mlx);
+	if (stat > 1)
+	{
+		mlx_delete_image(cub->mlx, cub->map.east);
+		mlx_delete_image(cub->mlx, cub->map.west);
+		mlx_delete_image(cub->mlx, cub->map.north);
+		mlx_delete_image(cub->mlx, cub->map.south);
+	}
 	mlx_close_window(cub->mlx);
 	free_map(&(cub->map));
 	exit (stat);
